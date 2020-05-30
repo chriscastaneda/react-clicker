344ee9e68eeb17b73b704dfac4269941c93e2e41
chris castaneda <chris.j.castaneda@gmail.com>
2020-05-29 18:35:19 -0700

    react-clicker project, CatFactory contents, headers

---
+++ extras/lecture-CatFactory/react-app/src/components/content-component/content.component.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './content.component.css';
+
+const ContentComponent: React.FC = () => {
+    /**Output to inventory */
+    const [excavators, setExcavators] = useState(0);
+    const [catapults, setCatapults] = useState(0);
+
+    /**input to manufacture */
+    const [excavatorsInput, setExcavatorsInput] = useState(0);
+    const [catapultsInput, setCatapultsInput] = useState(0);
+    
+    /**Submit */
+    const manufacture = () => {
+        setExcavators(excavators + excavatorsInput);
+        setCatapults(catapults + catapultsInput) ;
+        setExcavatorsInput(0);
+        setCatapultsInput(0);
+    }
+
+    return(
+        <main id="content-component">
+            <section id="inventory-container">
+                <h2>inventory</h2>
+                <ol>
+                    <li>{excavators} Excavators</li>
+                    <li>{catapults} Catapults</li>
+                </ol>
+            </section>
+            <section id="manufacture-container">
+                <h2>manufacture</h2>
+                <form>
+                    <fieldset>
+                        <label>
+                            Excavators:
+                        <input value={excavatorsInput} onChange={(e) => setExcavatorsInput(Math.max(excavatorsInput, +e.target.value))} type="number"></input>
+                        </label>
+                    </fieldset>
+                    <fieldset>
+                        <label>
+                            Catapults:
+                            <input value={catapultsInput} onChange={(e) => setCatapultsInput(Math.max(catapultsInput, +e.target.value))} type="number"></input>
+                        </label>
+                    </fieldset>
+                </form>
+                <button onClick={ () => manufacture()}>submit</button>
+            </section>
+        </main>
+    );
+}
+
+export default ContentComponent;
---
+++ extras/lecture-CatFactory/react-app/src/components/header-component/header.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.component.css';
+
+const HeaderComponent: React.FC = () => {
+    return (
+    <header id="header-component">
+        <h1>CAT Factory</h1>
+    </header>
+    );
+}
+
+export default HeaderComponent; 
